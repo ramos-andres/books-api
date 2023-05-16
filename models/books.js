24929737c8cc09e9f750f0c50b20f90b3d28732f
ramos-andres <102202582+ramos-andres@users.noreply.github.com>
2023-05-16 15:14:32 -0700

    Added routes

---
+++ models/books.js
@@ -0,0 +1,16 @@
+//dependencies
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+//schema
+const bookSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    year: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    imageURL: { type: String, required: true },
+});
+
+//model and export
+const Book = mongoose.model('Book', bookSchema)
+module.exports = Book
